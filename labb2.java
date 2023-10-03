968fd1cda7be7f1193399abfd20c339883d7737d
Jarrizzo <123414006+Jarrizzo@users.noreply.github.com>
2023-10-03 10:10:35 +0200

    Update labb2.java

--- labb2.java
+++ labb2.java
@@ -5,7 +5,7 @@ public class labb2 {
 
     public static void main(String[] args) throws FileNotFoundException  {
         
-        String pifPath = "pif_1.txt";
+        String pifPath = "pif_2.txt";
         ArrayList <String> SplitFile = new ArrayList<String>();
         BufferedReader read = null;
         try {
@@ -50,10 +50,56 @@ public class labb2 {
     public static  int FirstComeFirstServe(ArrayList <String> InputArray){
 
         int length = InputArray.size();
-        System.out.println(length);
+        ArrayList <Integer> tmpArray = new ArrayList<Integer>();
+        ArrayList <String> FinalArray = new ArrayList<String>();
+        int TurnAroudTime = 0;
+        int WaitingTime = 0;
+
+
+         for(int j = 0; j < length-3;j+=3){
+            for(int i = 0;i < length-j-3;i+=3){
+                if(Integer.parseInt(InputArray.get(i+1)) > Integer.parseInt(InputArray.get(i+4))){
+                    String tmp0 = InputArray.get(i);
+                    String tmp1 = InputArray.get(i+1);
+                    String tmp2 = InputArray.get(i+2);
+                    InputArray.set(i,InputArray.get(i+3));
+                    InputArray.set(i+1,InputArray.get(i+4));
+                    InputArray.set(i+2,InputArray.get(i+5));
+                    InputArray.set(i+3,tmp0);
+                    InputArray.set(i+4,tmp1);
+                    InputArray.set(i+5,tmp2);
+                }
+            }
+        }
+        for(String i: InputArray){
+            tmpArray.add(Integer.parseInt(i));
+        }
+        for(int i = 0; i < length; i+=3){
+            if(i == 0){
+                WaitingTime = 0;
+                TurnAroudTime = WaitingTime + tmpArray.get(i+2);
+            }
+            else if(tmpArray.get(i+1) >= tmpArray.get(i-1)+tmpArray.get(i-2)){
+                WaitingTime = 0;
+                TurnAroudTime = WaitingTime + tmpArray.get(i+2); 
+            }
+            else{
+                WaitingTime = (tmpArray.get(i-2) + Integer.parseInt(FinalArray.get(i-1)) - tmpArray.get(i+1));
+                TurnAroudTime =WaitingTime + tmpArray.get(i+2);
+            }
+
+            FinalArray.add(InputArray.get(i));
+            FinalArray.add(Integer.toString(WaitingTime));
+            TurnAroudTime = WaitingTime + Integer.parseInt(InputArray.get(i+2));
+            FinalArray.add(Integer.toString(TurnAroudTime));
+        }
+
+        for(String i:FinalArray)
+            System.out.print("  "+i);
 
     return 0;
     }
+
 }
 
 
